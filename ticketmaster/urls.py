4765faab4f7d0dcee784d3a1c274ff47d5a190db
ABaerga2002 <a.baerga2002@gmail.com>
2023-11-29 14:59:11 -0500

    Re-edited files

--- ticketmaster/urls.py
+++ ticketmaster/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from ticketmaster import views
 
 urlpatterns = [
-    path('ticketmaster/', views.index, name='ticketmaster-index'),
+    path('', views.index, name='ticketmaster-index'),
 ]
