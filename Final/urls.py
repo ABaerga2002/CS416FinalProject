aef36077e8064bb3b76d0bb41417e117d3388ed5
ABaerga2002 <a.baerga2002@gmail.com>
2023-12-12 23:47:51 -0500

    Final changes to local project

--- Final/urls.py
+++ Final/urls.py
@@ -20,5 +20,5 @@ from ticketmaster import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ticketmaster/', include('ticketmaster.urls'))
+    path('', include('ticketmaster.urls'))
 ]
